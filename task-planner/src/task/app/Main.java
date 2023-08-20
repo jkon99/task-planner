da41f01951efe1e724c197bb878d7e58248bc2d0
Jonathan <Jonathan@JON-PC>
2023-08-19 20:48:19 -0400

    comments

--- task-planner/src/task/app/Main.java
+++ task-planner/src/task/app/Main.java
@@ -1,22 +1,5 @@
 package task.app;
 
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.Border;
-import java.awt.Color;
-import java.awt.BorderLayout;
-import java.awt.Component;
 
 public class Main {
 
@@ -29,3 +12,11 @@ public class Main {
 @interface override {
 	
 }
+
+/*things to get done will be written below */
+
+// in Task class, create a way to undo the "done" option
+// adjust colors
+
+// can save progress
+// can import and export list
